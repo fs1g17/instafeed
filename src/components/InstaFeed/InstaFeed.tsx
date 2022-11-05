d123b1c72df14f6fe781a7cc953ca281fe0ae25c
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-11-05 18:36:53 +0000

    Remove access token and user id dependency

--- src/components/InstaFeed/InstaFeed.tsx
+++ src/components/InstaFeed/InstaFeed.tsx
@@ -13,14 +13,11 @@ export interface InstaItem {
 const InstaFeed = () => {
   const [instaItems, setInstaItems] = useState<InstaItem[]>([]);
 
-  const userId = process.env.REACT_APP_USER_ID;
-  const accessToken = process.env.REACT_APP_ACCESS_CODE;
-
-  const instaUrl = `https://graph.instagram.com/${userId}/media?access_token=${accessToken}`;
+  const instaUrl = "https://europe-west2-insta-query.cloudfunctions.net/test";
 
   useEffect(() => {
     const fetchMedia = async (id: string) => {
-      const mediaUrl = `https://graph.instagram.com/${id}?access_token=${accessToken}&fields=media_url,permalink,thumbnail_url,media_type`;
+      const mediaUrl = `https://europe-west2-insta-query.cloudfunctions.net/test2?id=${id}`;
 
       const res = await fetch(mediaUrl);
       const json = (await res.json());
@@ -38,11 +35,6 @@ const InstaFeed = () => {
       return instaItem;
     }
     const doFetch = async () => {
-      if(!userId || !accessToken) {
-
-        console.log("userId or accessToken is undefined: ", {userId, accessToken});
-        return;
-      }
       const res = await fetch(instaUrl);
       const json = (await res.json()).data;
 
