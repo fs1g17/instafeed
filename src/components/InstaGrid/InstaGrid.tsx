16dbd70ee74fec8cccc8a18a8bd820c50e992165
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-11-08 17:29:01 +0000

    Clean up code
    Use styled components for grid.

--- src/components/InstaGrid/InstaGrid.tsx
+++ src/components/InstaGrid/InstaGrid.tsx
@@ -3,27 +3,47 @@ import React from 'react';
 import Skeleton from "@mui/material/Skeleton";
 
 import { InstaItem, MediaItem } from '../../components';
+import styled from '@emotion/styled';
 
 interface InstaGridProps {
   items: InstaItem[];
 }
 
-const InstaGrid = ({ items }: InstaGridProps) => {
+const StyledGrid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 2%;
+`
 
+const InstaGrid = ({ items }: InstaGridProps) => {
   if (items.length === 0) {
     return (
-      <div style={{ width: '100%', display: "grid", gridTemplateColumns: "repeat(3,1fr)", gridTemplateRows: "repeat(3,1fr)", rowGap: "2%", columnGap: "2%" }}>
-        {(new Array(9)).fill(<Skeleton variant='rectangular' sx={{ display: 'block', objectFit: 'cover', width: '100%', height: '100%', aspectRatio: "1/ 1" }} />)}
-      </div>
+      <StyledGrid>
+        {(new Array(9)).fill(
+          <Skeleton
+            variant='rectangular'
+            sx={{
+              display: 'block',
+              objectFit: 'cover',
+              width: '100%',
+              height: '100%',
+              aspectRatio: "1/ 1"
+            }}
+          />
+        )}
+      </StyledGrid>
     )
   }
   return (
-    <div style={{ width: '100%', display: "grid", gridTemplateColumns: "repeat(3,1fr)", gridTemplateRows: "repeat(3,1fr)", rowGap: "2%", columnGap: "2%" }}>
+    <StyledGrid>
       {items.map(item => (
         <MediaItem item={item} />
       ))}
-    </div>
+    </StyledGrid>
   );
 };
 
 export default InstaGrid;
+
